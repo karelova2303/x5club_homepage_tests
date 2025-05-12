995f97935cb4522754bf06a18321ffd09202d54d
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-05-12 23:28:30 +0300

    добавление page_footer.py

---
+++ x5club_homepage/pages/page_footer.py
@@ -0,0 +1,42 @@
+import allure
+from selene import browser, have, command, be
+
+
+class PageFooter():
+    def __init__(self):
+        self.footer = browser.element('footer')
+
+        self.connect_with_us_button = self.footer.element('button')
+        self.title_popup = browser.element('.tp-h2-medium')
+
+        self.vk_icon = browser.element('[href="https://vk.com/x5club"]')
+        self.t_icon = browser.element('[href="https://t.me/x5clubru"]')
+        self.dzen_icon = browser.element('[href="https://dzen.ru/x5club"]')
+        self.ok_icon = browser.element('[href="https://ok.ru/group/70000002639516"]')
+
+    def scroll_page_to_footer(self):
+        self.footer.perform(command.js.scroll_into_view)
+
+    def should_be_contact_us(self, value):
+        with allure.step('Скроллим страницу вниз'):
+            self.scroll_page_to_footer()
+        with allure.step('Кликаем кнопку "Свяжитесь с нами"'):
+            self.connect_with_us_button.click()
+        with allure.step('Проверяем название попапа'):
+            self.title_popup.should(have.exact_text(value))
+
+    def should_be_clickable_vk(self):
+        self.scroll_page_to_footer()
+        self.vk_icon.should(be.clickable)
+
+    def should_be_clickable_telegram(self):
+        self.scroll_page_to_footer()
+        self.t_icon.should(be.clickable)
+
+    def should_be_clickable_dzen(self):
+        self.scroll_page_to_footer()
+        self.dzen_icon.should(be.clickable)
+
+    def should_be_clickable_ok(self):
+        self.scroll_page_to_footer()
+        self.ok_icon.should(be.clickable)
