9c346b725026c9f0d732c7aef90e2e811ca2946b
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-05-13 22:16:44 +0300

    добавлен выбор браузеров и версий

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,27 +9,55 @@ from selenium.webdriver.chrome.options import Options
 
 from utils import attach
 
+DEFAULT_BROWSER = 'firefox'
 DEFAULT_BROWSER_VERSION = '125.0'
 
+
 def pytest_addoption(parser):
-    parser.addoption('--browser_version', default='125.0')
+    parser.addoption(
+        '--browser_name',
+        default=DEFAULT_BROWSER,
+        help='web: browser (chrome | firefox)'
+    )
+
+    parser.addoption(
+        '--browser_version',
+        default=DEFAULT_BROWSER_VERSION,
+        help='web: browser version (if chrome: 127.0, 128.0; firefox: 125.0, 124.0)'
+    )
+
 
 @pytest.fixture(scope='session', autouse=True)
 def load_env():
     load_dotenv()
 
+
 @pytest.fixture(scope='session')
-def open_browser(request):
-    browser_version = request.config.getoption('--browser_version')
+def get_option_browser_name(request):
+    return request.config.getoption('--browser_name')
+
+
+@pytest.fixture(scope='session')
+def get_option_browser_version(request):
+    return request.config.getoption('--browser_version')
+
+
+@pytest.fixture(scope='session')
+def setup_browser(get_option_browser_version, get_option_browser_name):
+    browser_name = get_option_browser_name
+    browser_name = browser_name if browser_name != '' else DEFAULT_BROWSER
+
+    browser_version = get_option_browser_version
     browser_version = browser_version if browser_version != '' else DEFAULT_BROWSER_VERSION
+
     options = Options()
     selenoid_capabilities = {
-            'browserName': 'firefox',
-            'browserVersion': browser_version,
-            'selenoid:options': {
-                'enableVNC': True,
-                'enableVideo': True,
-                'screenResolution': '1920x1080x24'
+        'browserName': browser_name,
+        'browserVersion': browser_version,
+        'selenoid:options': {
+            'enableVNC': True,
+            'enableVideo': True,
+            'screenResolution': '1920x1080x24'
         }
     }
     options.capabilities.update(selenoid_capabilities)
@@ -44,7 +72,6 @@ def open_browser(request):
     )
 
     browser.config.driver = driver
-
     browser.config.base_url = 'https://x5club.ru'
     browser.driver.maximize_window()
 
@@ -54,7 +81,7 @@ def open_browser(request):
 
 
 @pytest.fixture(scope='function')
-def open_homepage(open_browser):
+def open_homepage(setup_browser):
     browser.open('/')
 
     yield browser
