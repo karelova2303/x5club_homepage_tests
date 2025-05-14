06c94e8fdaa4f14eff102ee9d50a5a0cbca7ddf9
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-05-14 21:16:34 +0300

    переписаны allure.step

--- x5club_homepage/pages/page_footer.py
+++ x5club_homepage/pages/page_footer.py
@@ -14,37 +14,32 @@ class PageFooter():
         self.dzen_icon = browser.element('[href="https://dzen.ru/x5club"]')
         self.ok_icon = browser.element('[href="https://ok.ru/group/70000002639516"]')
 
+    def open_homepage(self):
+        browser.open('/')
+
     def scroll_page_to_footer(self):
         self.footer.perform(command.js.scroll_into_view)
 
     def should_be_contact_us(self, value):
+        with allure.step('Открываем Главную страницу'):
+            self.open_homepage()
         with allure.step('Скроллим страницу вниз'):
             self.scroll_page_to_footer()
         with allure.step('Кликаем кнопку "Свяжитесь с нами"'):
             self.connect_with_us_button.click()
-        with allure.step('Проверяем название попапа'):
+        with allure.step('Проверяем название всплывающего окна'):
             self.title_popup.should(have.exact_text(value))
 
-    def should_be_clickable_vk(self):
+    def should_be_clickable_social_network_icon(self):
+        with allure.step('Открываем Главную страницу'):
+            self.open_homepage()
         with allure.step('Скроллим страницу вниз'):
             self.scroll_page_to_footer()
-        with allure.step('Проверяем кликабельность иконки "ВКонтакте"'):
+        with allure.step('Иконка "ВКонтакте" кликабельна'):
             self.vk_icon.should(be.clickable)
-
-    def should_be_clickable_telegram(self):
-        with allure.step('Скроллим страницу вниз'):
-            self.scroll_page_to_footer()
-        with allure.step('Проверяем кликабельность иконки "Телеграм"'):
+        with allure.step('Иконка "Телеграм" кликабельна'):
             self.t_icon.should(be.clickable)
-
-    def should_be_clickable_dzen(self):
-        with allure.step('Скроллим страницу вниз'):
-            self.scroll_page_to_footer()
-        with allure.step('Проверяем кликабельность иконки "Дзен"'):
+        with allure.step('Иконка "Дзен" кликабельная'):
             self.dzen_icon.should(be.clickable)
-
-    def should_be_clickable_ok(self):
-        with allure.step('Скроллим страницу вниз'):
-            self.scroll_page_to_footer()
-        with allure.step('Проверяем кликабельность иконки "Одноклассники"'):
+        with allure.step('Иконка "Одноклассники" кликабельна'):
             self.ok_icon.should(be.clickable)
