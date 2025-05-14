00c0b6abe50f9c843b97b4036ccde7d704de2cd1
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-05-14 21:17:24 +0300

    переписаны allure.step

--- tests/test_header_x5club.py
+++ tests/test_header_x5club.py
@@ -7,47 +7,53 @@ page_header = PageHeader()
 
 @allure.tag('Web', 'Prod')
 @allure.label('owner', 'Karelova Ekaterina')
-@allure.title('Проверка работы линка "О Х5 Клубе" в хедере')
+@allure.suite('Проверка кликабельности линков и кнопок в хедере')
+@allure.title('Отображение контента при клике "О Х5 Клубе"')
 @allure.link('https://x5club.ru')
-def test_be_clickable_link_about(open_homepage):
+def test_be_clickable_link_about(setup_browser):
     page_header.should_be_clickable_link_about()
 
 
 @allure.tag('Web', 'Prod')
 @allure.label('owner', 'Karelova Ekaterina')
-@allure.title('Проверка работы линка "Как стать участником"  в хедере')
+@allure.suite('Проверка кликабельности линков и кнопок в хедере')
+@allure.title('Отображение контента при клике "Как стать участником"')
 @allure.link('https://x5club.ru')
-def test_be_clickable_link_participant(open_homepage):
+def test_be_clickable_link_participant(setup_browser):
     page_header.should_be_clickable_link_participant()
 
 
 @allure.tag('Web', 'Prod')
 @allure.label('owner', 'Karelova Ekaterina')
-@allure.title('Проверка работы линка "Партнеры"  в хедере')
+@allure.suite('Проверка кликабельности линков и кнопок в хедере')
+@allure.title('Отображение контента при клике "Партнеры"')
 @allure.link('https://x5club.ru')
-def test_be_clickable_link_partners(open_homepage):
+def test_be_clickable_link_partners(setup_browser):
     page_header.should_be_clickable_link_partners()
 
 
 @allure.tag('Web', 'Prod')
 @allure.label('owner', 'Karelova Ekaterina')
-@allure.title('Проверка работы линка "Мобильные приложения"  в хедере')
+@allure.suite('Проверка кликабельности линков и кнопок в хедере')
+@allure.title('Отображение контента при клике "Мобильные приложения"')
 @allure.link('https://x5club.ru')
-def test_be_clickable_link_mobile_app(open_homepage):
+def test_be_clickable_link_mobile_app(setup_browser):
     page_header.should_be_clickable_link_mobile_app()
 
 
 @allure.tag('Web', 'Prod')
 @allure.label('owner', 'Karelova Ekaterina')
-@allure.title('Проверка работы линка "Вопросы и ответы"  в хедере')
+@allure.suite('Проверка кликабельности линков и кнопок в хедере')
+@allure.title('Отображение контента при клике "Вопросы и ответы"')
 @allure.link('https://x5club.ru')
-def test_common_be_clickable_link_question(open_homepage):
+def test_common_be_clickable_link_question(setup_browser):
     page_header.should_be_clickable_link_common_question()
 
 
 @allure.tag('Web', 'Prod')
 @allure.label('owner', 'Karelova Ekaterina')
-@allure.title('Проверка работы кнопки "Войти"  в хедере')
+@allure.suite('Проверка кликабельности линков и кнопок в хедере')
+@allure.title('Загрузка страницы авторизации при клике на кнопку "Войти"')
 @allure.link('https://x5club.ru')
-def test_be_clickable_login_button(open_homepage):
+def test_be_clickable_login_button(setup_browser):
     page_header.should_be_clickable_login_button()
