55fcd0f49e886735af44c2e193a84c4ecc51ce09
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-05-14 21:16:42 +0300

    переписаны allure.step

--- x5club_homepage/pages/page_header.py
+++ x5club_homepage/pages/page_header.py
@@ -22,38 +22,53 @@ class PageHeader():
         self.login_button = browser.element('[data-testid="loginButton"]')
         self.form_login = browser.element('#kc-form-login')
 
+    def open_homepage(self):
+        browser.open('/')
+
     def should_be_clickable_link_about(self):
+        with allure.step('Открываем Главную страницу'):
+            self.open_homepage()
         with allure.step('Кликаем линк "О Х5 Клубе"'):
             self.link_about.click()
-        with allure.step('Проверяем отображение контента "О Х5 Клубе"'):
+        with allure.step('Отображается контент "О Х5 Клубе"'):
             self.about_content_card.should(be.visible)
 
     def should_be_clickable_link_participant(self):
+        with allure.step('Открываем Главную страницу'):
+            self.open_homepage()
         with allure.step('Кликаем линк "Как стать участником"'):
             self.link_participant.click()
-        with allure.step('Проверяем отображение контента "Как стать участником"'):
+        with allure.step('Отображается контент "Как стать участником"'):
             self.participant_content_card.should(be.visible)
 
     def should_be_clickable_link_partners(self):
+        with allure.step('Открываем Главную страницу'):
+            self.open_homepage()
         with allure.step('Кликаем линк "Партнеры"'):
             self.link_partners.click()
-        with allure.step('Проверяем отображение контента "Партнеры"'):
+        with allure.step('Отображается контент "Партнеры"'):
             self.partners_content_card.should(be.visible)
 
     def should_be_clickable_link_mobile_app(self):
+        with allure.step('Открываем Главную страницу'):
+            self.open_homepage()
         with allure.step('Кликаем линк "Мобильные приложения"'):
             self.link_mobile_app.click()
-        with allure.step('Проверяем отображение контента "Мобильные приложения"'):
+        with allure.step('Отображается контент "Мобильные приложения"'):
             self.mobile_app_content_card.should(be.visible)
 
     def should_be_clickable_link_common_question(self):
+        with allure.step('Открываем Главную страницу'):
+            self.open_homepage()
         with allure.step('Кликаем линк "Вопросы и ответы"'):
             self.link_common_question.click()
-        with allure.step('Проверяем отображение контента "Вопросы и ответы"'):
+        with allure.step('Отображается контент "Вопросы и ответы"'):
             self.common_question_content_card.should(be.visible)
 
     def should_be_clickable_login_button(self):
+        with allure.step('Открываем Главную страницу'):
+            self.open_homepage()
         with allure.step('Кликаем кнопку "Войти"'):
             self.login_button.click()
-        with allure.step('Проверяем открытие страницы авторизации'):
+        with allure.step('Отображается страница авторизации'):
             self.form_login.should(be.visible)
