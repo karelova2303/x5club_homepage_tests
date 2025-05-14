fa1455fbe30d4c4a6ee9f098012e7916eab2b6a7
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-05-14 19:52:06 +0300

    обновлен браузер на Chrome

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,27 +3,20 @@ import os
 import pytest
 from dotenv import load_dotenv
 
-from selene import browser, Browser, Config
+from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 from utils import attach
 
-DEFAULT_BROWSER = 'firefox'
-DEFAULT_BROWSER_VERSION = '125.0'
+DEFAULT_BROWSER_VERSION = '128.0'
 
 
 def pytest_addoption(parser):
     parser.addoption(
-        '--browser_name',
-        default=DEFAULT_BROWSER,
-        help='web: browser (chrome | firefox)'
-    )
-
-    parser.addoption(
         '--browser_version',
         default=DEFAULT_BROWSER_VERSION,
-        help='web: browser version (if chrome: 127.0, 128.0; firefox: 125.0, 124.0)'
+        help='browser version (firefox: 128.0, 127.0)'
     )
 
 
@@ -33,26 +26,18 @@ def load_env():
 
 
 @pytest.fixture(scope='session')
-def get_option_browser_name(request):
-    return request.config.getoption('--browser_name')
-
-
-@pytest.fixture(scope='session')
 def get_option_browser_version(request):
     return request.config.getoption('--browser_version')
 
 
 @pytest.fixture(scope='session')
-def setup_browser(get_option_browser_version, get_option_browser_name):
-    browser_name = get_option_browser_name
-    browser_name = browser_name if browser_name != '' else DEFAULT_BROWSER
-
+def setup_browser(get_option_browser_version):
     browser_version = get_option_browser_version
     browser_version = browser_version if browser_version != '' else DEFAULT_BROWSER_VERSION
 
     options = Options()
     selenoid_capabilities = {
-        'browserName': browser_name,
+        'browserName': 'chrome',
         'browserVersion': browser_version,
         'selenoid:options': {
             'enableVNC': True,
@@ -87,6 +72,6 @@ def open_homepage(setup_browser):
     yield browser
 
     attach.add_screenshot(browser)
-    # attach.add_logs(browser)
+    attach.add_logs(browser)
     attach.add_html(browser)
     attach.add_video(browser)
